95af9e90a6c9d05524369bcfcb6a2130710da157
KhaLift <102684091+KhaLift@users.noreply.github.com>
2021-02-28 11:57:27 +0000

    add travis-ci configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = fu
+module.exports = function (config) {
+  config.set({
+    basePath
