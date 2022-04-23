7d085f52b8c9410c8a29ff010c28f8cd5f4f0bfd
KhaLift <102684091+KhaLift@users.noreply.github.com>
2022-04-23 18:34:16 +0000

    production

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,4 +6,6 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-     
+      declarations: [
+        AppComponent
+   
