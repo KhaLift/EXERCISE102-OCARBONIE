b6e8c984215a185499ca48b0c14e64dc9873b695
KhaLift <102684091+KhaLift@users.noreply.github.com>
2022-05-20 17:25:23 +0000

    readme modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,4 +14,5 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponen
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElem
