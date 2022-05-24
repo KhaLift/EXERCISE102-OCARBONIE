7edcdca24ada59767c0f3363f3217a32815127ed
KhaLift <102684091+KhaLift@users.noreply.github.com>
2022-05-24 19:49:08 +0000

    deleted stuff

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,4 +16,7 @@ describe('AppComponent', () => {
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).to
+    expect(app).toBeTruthy();
+  }));
+
+  it(`should 
