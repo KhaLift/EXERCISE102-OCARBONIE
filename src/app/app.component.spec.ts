4c0b52c201522f18b5e67c51748413d5257f9563
KhaLift <102684091+KhaLift@users.noreply.github.com>
2022-08-25 21:29:49 +0000

    Release version 0.5.1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,4 +28,4 @@ describe('AppComponent', () => {
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-   
+    const compiled = fixture.debug
