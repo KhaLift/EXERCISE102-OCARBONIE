7b610c5a5376f0cfafee80e38d0447bc79ebc582
KhaLift <102684091+KhaLift@users.noreply.github.com>
2022-09-10 13:06:10 +0000

    Released v0.1.4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,4 +29,4 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compile
+    expect(compiled.querySelector('h1').textContent).toContain
