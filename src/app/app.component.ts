fc13a21b98c32ccdf689b2ca42a158d97bb48b16
KhaLift <102684091+KhaLift@users.noreply.github.com>
2022-09-12 16:12:08 +0000

    Create the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,2 +1,4 @@
 // src/app/app.component.ts
-import { Component, trigger, state, style, animate, transition } fro
+import { Component, trigger, state, style, animate, transition } from '@angular/core';
+
+import { StockService } from './s
