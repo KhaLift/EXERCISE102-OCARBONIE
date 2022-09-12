6ea0253d45b5f0ab7de11785764f3e7e0e0b5e79
KhaLift <102684091+KhaLift@users.noreply.github.com>
2022-09-12 20:40:17 +0000

    Added gulpfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 // src/app/app.component.ts
 import { Component, trigger, state, style, animate, transition } from '@angular/core';
 
-import { StockService } from './s
+import { StockService } from './stock.service';
+
+@Component({
+  selector: 'app
