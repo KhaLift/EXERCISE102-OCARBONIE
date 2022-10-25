f2957665e570cd49a62039f43db5cd7fe3a765c3
KhaLift <102684091+KhaLift@users.noreply.github.com>
2022-10-25 20:33:44 +0000

    Release v1.8.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,4 +2,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/htt
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.co
