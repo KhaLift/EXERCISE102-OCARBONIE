d779659520c4a63f21f37b7fca83b3f4cbe01a3d
KhaLift <102684091+KhaLift@users.noreply.github.com>
2022-11-14 17:36:28 +0000

    bumped dragula version

--- src/app/stock.service.spec.ts
+++ src/app/stock.service.spec.ts
@@ -4,4 +4,6 @@ import { TestBed, async, inject } from '@angular/core/testing';
 import { StockService } from './stock.service';
 
 describe('StockService', () => {
-  beforeEach(() => 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+ 
