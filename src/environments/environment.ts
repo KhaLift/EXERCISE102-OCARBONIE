9f3881506cebae2b5c1a9cd0a3b9aa130ce76ad3
KhaLift <102684091+KhaLift@users.noreply.github.com>
2023-01-30 22:23:03 +0000

    Clean up some comments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,3 @@
 // The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment w
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `
