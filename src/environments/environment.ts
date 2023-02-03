516be862d81b2cd4336ebef703689ab8f68b54d3
KhaLift <102684091+KhaLift@users.noreply.github.com>
2023-02-03 19:03:55 +0000

    remove test folder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,5 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
-exp
+export const environment = {
+  produ
