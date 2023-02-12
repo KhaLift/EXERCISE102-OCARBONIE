608c4663d1d05905a5d72dd41f2db23536155e9e
KhaLift <102684091+KhaLift@users.noreply.github.com>
2023-02-12 11:53:54 +0000

    docs: cleanup

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
-if (environm
+if (environment.production) {
+  enableProdMode();
+}
