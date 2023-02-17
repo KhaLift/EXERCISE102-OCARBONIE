59146df1db478431f3efa68647169c905b558883
KhaLift <102684091+KhaLift@users.noreply.github.com>
2023-02-17 17:58:29 +0000

    Remove boilerplate

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -4,4 +4,5 @@ import 'core-js/es6/symbol';
 import 'core-js/es6/object';
 import 'core-js/es6/function';
 import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-
+import 'core-js/es6/parse-float';
+import 'core-js/es6
