dd564f3a8c8973b7473eed1e5da02630bff4437a
KhaLift <102684091+KhaLift@users.noreply.github.com>
2023-02-28 18:50:07 +0000

    Update terms

--- src/test.ts
+++ src/test.ts
@@ -6,4 +6,5 @@ import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
 import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/a
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-asyn
