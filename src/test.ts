fde61944470aa632459f46a23b3a1d4083760439
KhaLift <102684091+KhaLift@users.noreply.github.com>
2023-03-05 05:33:58 +0000

    update registre Sénat

--- src/test.ts
+++ src/test.ts
@@ -7,4 +7,5 @@ import 'zone.js/dist/proxy.js';
 import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-asyn
+import 'zone.js/dist/fake-async-test';
+import { getTestBed } from '@angu
